c1ecf7ea405429b168d92b328cec92745876c719
modfun <modfun95@gmail.com>
2021-11-20 13:08:52 -0500

    Add two options of interactions and implementing selectMode

--- js/script.js
+++ js/script.js
@@ -4,28 +4,45 @@ const container = document.getElementsByClassName("container")[0];
 const panel = document.querySelector('.panel');
 const rows = 16;
 const cols = 16;
+const buttonClasses = ['hoverMode', 'selectMode'];
+const buttonText = ['Hover Mode', 'Select Mode'];
+
+let color = 'blue';
 let currentMode = null;
 let currentEvent = '';
 
 
-console.log( "[Created] " + createGrid( rows, cols, container) + " Node");
-document.body.appendChild( container);
+console.log( "[Created] " + 
+    createGrid( rows, cols, container) + " Squares");
+console.log("[Created] " + 
+    createButtons( buttonClasses, buttonText, panel) + " Buttons");
+// document.body.appendChild( container);
 
-color = 'blue';
-container.addEventListener( 'mousemove', hoverMode.bind(null, color));
+let hoverModeBind = hoverMode.bind( null, color);
+let selectModeBind = selectMode.bind( null, color);
+
+container.addEventListener( 'mousemove', hoverModeBind);
+// container.removeEventListener( 'mousemove', hoverModeBind);
 currentEvent = 'mousemove';
-currentMode = hoverMode;
+currentMode = hoverModeBind;
 
-panel.addEventListener( 'click', getMode.bind(null, color, container)); 
+panel.addEventListener( 'click', getMode.bind(null, hoverModeBind, selectModeBind, color, container)); 
 
-function getMode( color, container, event) {
+function getMode( hoverModeBind, selectModeBind, color, container, event) {
+    console.log(event.target.className + ':1');
     if (event.target.className === 'hoverMode') {
+        console.log(currentEvent + ":2");
         container.removeEventListener( currentEvent, currentMode);
-        hoverMode( color, event);
+        container.addEventListener( 'mousemove', hoverModeBind);
+        currentEvent = 'mousemove';
+        currentMode = hoverModeBind;
     }
     else if (event.target.className === 'selectMode') {
+        console.log(currentEvent + ":3");
         container.removeEventListener( currentEvent, currentMode);
-        selectMode( color, event);
+        container.addEventListener( 'click', selectModeBind);
+        currentEvent = 'click';
+        currentMode = selectModeBind;
     }
 }
 
@@ -45,14 +62,22 @@ function createGrid( rowSize, colSize, target) {
     return rowSize*colSize;
 }
 
+function createButtons( classes, text, target) {
+    for (let i = 0; i < classes.length; ++i) {
+        const button = document.createElement('button');
+        button.textContent = text[i]
+        button.classList.add(classes[i]);
+        target.appendChild(button);
+    }
+    return classes.length;
+}
+
 function hoverMode( color, event) {
-    console.log(event.target);
+    console.log('hover mode');
     event.target.style['background-color'] = color;
 }
 
-function clearGrid() {
-}
-
-function selectMode( event, color) {
-    hoverMode( color, event);
+function selectMode( color, event) {
+    console.log('selct mode');
+    event.target.style['background-color'] = color;
 }
