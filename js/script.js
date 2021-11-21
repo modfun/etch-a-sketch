7a55100b9c4af861f61f5d71374ca871aee67eaf
modfun <modfun95@gmail.com>
2021-11-21 10:49:26 -0500

    Extend the fuctionality of selectMode to include an area of effect

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ const buttonClasses = ['hoverMode', 'selectMode', 'eraserMode'];
 const buttonText = ['Hover Mode', 'Select Mode', 'Eraser Mode'];
 
 let color = 'blue';
+let eraserColor = 'white';
+let brushSize = 4;
 let currentMode = null;
 let currentEvent = '';
 
@@ -24,8 +26,8 @@ console.log("[Created] " +
 //store the reference to use with removeEventListener() which
 //only accepts the same reference that addEventListener() took.
 let hoverModeBind = hoverMode.bind( null, color);
-let selectModeBind = selectMode.bind( null, color);
-let eraserModeBind = selectMode.bind( null, 'white');
+let selectModeBind = selectMode.bind( null, color, brushSize, rows, cols);
+let eraserModeBind = selectMode.bind( null, eraserColor, brushSize, rows, cols);
 
 container.addEventListener( 'mousemove', hoverModeBind);
 currentEvent = 'mousemove';
@@ -90,7 +92,44 @@ function hoverMode( color, event) {
     event.target.style['background-color'] = color;
 }
 
-function selectMode( color, event) {
+function selectMode( color, brushSize, rows, cols, event) {
     console.log('selct mode');
-    event.target.style['background-color'] = color;
+    if ( brushSize === 0) {
+        event.target.style['background-color'] = color;
+    }
+    else {
+        //get the coordinates of the current target square
+        let col = Array.prototype.indexOf.call(
+            event.target.parentNode.childNodes, event.target);
+        let row = Array.prototype.indexOf.call(
+            event.target.parentNode.parentNode.childNodes, event.target.parentNode);
+
+        //calculate the area of effect to perfurme the action
+        //make sure nothing goes out of bounds
+        const row_start = row - Math.trunc( brushSize / 2);
+        let row_end = row + Math.trunc( brushSize / 2);
+        row_end = ( row_end >= rows) ? rows - 1 : row_end; //>= because we start from zero
+
+        const col_start = col - Math.trunc( brushSize / 2);
+        let col_end = col + Math.trunc( brushSize / 2);
+        col_end = ( col_end >= cols) ? cols - 1 : col_end;
+        console.log(row);
+        console.log(col);
+
+        //start traversing according to the calculatd bounds
+        let y = ( row_start < 0) ? 0: row_start;    //prevent going offbounds
+        while ( y <= row_end) {
+            let rowNode = event.target.parentNode.parentNode.childNodes.item(y);
+            console.log(rowNode);
+
+            let x = ( col_start < 0) ? 0: col_start;
+            while ( x <= col_end) {
+                let square = rowNode.childNodes.item(x);
+                console.log(square);
+                square.style['background-color'] = color;
+                x++;
+            }
+            y++;
+        }
+    }
 }
